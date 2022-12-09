cb820ded33a7f0a49aaf188a5d597ea5c124c2c0
ViniciusGR797 <loritoamarelo@outlook.com>
2022-12-09 10:54:12 -0300

    add

--- entity/entity.go
+++ entity/entity.go
@@ -6,28 +6,52 @@ import (
 )
 
 type ProdutoInterface interface {
-	toString() string
+	String() string
 }
 
+// Estrutura de dados de Produto
 type Produto struct {
-	ID          int     `json:"id"`
-	Name        string  `json:"name"`
-	Code        string  `json:"code"`
-	Price       float64 `json:"price"`
-	CriadoEm    string  `json:"criado_em,omitempty"`
-	AtulizadoEm string  `json:"atulizado_em,omitempty"`
+	ID        int     `json:"id"`
+	Name      string  `json:"name"`
+	Code      string  `json:"code"`
+	Price     float64 `json:"price"`
+	CreatedAt string  `json:"created_at,omitempty"`
+	UpdatedAt string  `json:"updated_at,omitempty"`
 }
 
-func (p Produto) toString() string {
+// Método de produto - retorna string com json do produto ou erro
+func (p *Produto) String() string {
 	data, err := json.Marshal(p)
+
 	if err != nil {
-		log.Printf("Erro ao converter Produto para String: \n--> %v", err)
+		log.Println("error to convert Produto to JSON")
+		log.Println(err.Error())
+		return ""
 	}
 
 	return string(data)
 }
 
-func NovoProduto(nome, code string, price float64) *Produto {
+// Estrutura de dados para lista de Produtos
+type ProdutoList struct {
+	List []*Produto `json:"list"`
+}
+
+// Método de ProdutoList - retorna string com json da lista de produtos ou erro
+func (pl *ProdutoList) String() string {
+	data, err := json.Marshal(pl)
+
+	if err != nil {
+		log.Println("error to convert ProdutoList to JSON")
+		log.Println(err.Error())
+		return ""
+	}
+
+	return string(data)
+}
+
+// Construtor de Produto - recebe dados no parâmetro e transforma em um produto
+func NewProduto(nome, code string, price float64) *Produto {
 	return &Produto{
 		Name:  nome,
 		Code:  code,
@@ -35,33 +59,24 @@ func NovoProduto(nome, code string, price float64) *Produto {
 	}
 }
 
-type ListaDeProduto struct {
-	List []*Produto `json:"lista"`
-}
-
-func (prodLista ListaDeProduto) toString() string {
-	data, err := json.Marshal(prodLista)
-	if err != nil {
-		log.Printf("Erro ao converter Produto para String: \n--> %v", err)
-	}
-
-	return string(data)
-}
-
+// Estrutura de dados para user - usuário que usará o sistema
 type User struct {
 	Username string `json:"username"`
-	Senha    string `json:"senha"`
+	Password string `json:"password"`
 }
 
-func NovoAdmin() *User {
+// Construtor de User - terá o admin como usuário padrão do sistema
+func NewAdmin() *User {
 	return &User{
 		Username: "admin",
-		Senha:    "supersenha",
+		Password: "supersenha",
 	}
 }
 
+// Estrutura de dados para token - key de acesso ao sistema para estar autenticado
 type Token struct {
 	Token string `json:"token"`
 }
 
+// Token do user - trata de um token fake, pois ele é uma constante que não altera
 const USER_TOKEN = "fake-WzD5fqrlaAXLv26bpI0hxvAhDp7T1Bac"
