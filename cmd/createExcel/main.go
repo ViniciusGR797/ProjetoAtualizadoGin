4f33d2ea5723eeeb486642c0e5f91f540ffeb456
ViniciusGR797 <loritoamarelo@outlook.com>
2022-12-13 20:40:46 -0300

    Excel com bordas

--- cmd/createExcel/main.go
+++ cmd/createExcel/main.go
@@ -52,54 +52,51 @@ func CreateExcel(list_product *entity.ProdutoList) {
 	f := excelize.NewFile()
 	// Set value of a cell.
 
-	styleHeader, err := f.NewStyle(`{									
-									"font":{
-										"bold":true},
-									"fill":{
-										"type":"pattern","pattern":1,"color":["#1976d2"]},
-									"alignment":{
-										"horizontal":"center","wrap_text":false}}`)
+	styleHeader, err := f.NewStyle(&excelize.Style{
+		Border: []excelize.Border{
+			{Type: "left", Color: "#000000", Style: 2},
+			{Type: "top", Color: "#000000", Style: 2},
+			{Type: "bottom", Color: "#000000", Style: 2},
+			{Type: "right", Color: "#000000", Style: 2},
+		},
+		Fill: excelize.Fill{
+			Type:    "pattern",
+			Pattern: 1,
+			Color:   []string{"#6959CD"},
+		},
+		Alignment: &excelize.Alignment{
+			Horizontal: "center",
+			WrapText:   true,
+		},
+		Font: &excelize.Font{
+			Bold:  true,
+			Color: "#FFFFFF",
+		},
+	})
 	if err != nil {
 		return
 	}
 
-	/*styleBold, err := f.NewStyle(`{"font":{"bold":true}}`)
-	if err != nil {
-		return
-	}*/
-
-	/*styleCenter, err := f.NewStyle(`{"alignment":{"horizontal":"center","wrap_text":false}}`)
-	if err != nil {
-		return
-	}*/
-
-	/*stylePrice, err := f.NewStyle(`{"number_format": 353}`)
-	if err != nil {
-		return
-	}*/
-
-	err = f.SetColWidth("Sheet1", "B", "F", 25)
-	if err != nil {
-		return
-	}
-	err = f.SetCellStyle("Sheet1", "A1", "F1", styleHeader)
+	styleBody, err := f.NewStyle(&excelize.Style{
+		Border: []excelize.Border{
+			{Type: "left", Color: "#000000", Style: 2},
+			{Type: "top", Color: "#000000", Style: 2},
+			{Type: "bottom", Color: "#000000", Style: 2},
+			{Type: "right", Color: "#000000", Style: 2},
+		},
+		Fill: excelize.Fill{
+			Type:    "pattern",
+			Pattern: 1,
+			Color:   []string{"#40E0D0"},
+		},
+		Alignment: &excelize.Alignment{
+			Horizontal: "center",
+			WrapText:   true,
+		},
+	})
 	if err != nil {
 		return
 	}
-	/*err = f.SetRowStyle("Sheet1", 1, 1, styleCenter)
-	if err != nil {
-		return
-	}*/
-
-	/*err = f.SetRowStyle("Sheet1", 2, 1000, styleCenter)
-	if err != nil {
-		return
-	}*/
-
-	/*err = f.SetColStyle("Sheet1", "D", stylePrice)
-	if err != nil {
-		return
-	}*/
 
 	f.SetCellValue("Sheet1", "A1", "ID")
 	f.SetCellValue("Sheet1", "B1", "Name")
@@ -112,9 +109,23 @@ func CreateExcel(list_product *entity.ProdutoList) {
 		f.SetCellValue("Sheet1", "A"+strconv.Itoa(i+2), p.ID)
 		f.SetCellValue("Sheet1", "B"+strconv.Itoa(i+2), p.Name)
 		f.SetCellValue("Sheet1", "C"+strconv.Itoa(i+2), p.Code)
-		f.SetCellValue("Sheet1", "D"+strconv.Itoa(i+2), p.Price)
+		f.SetCellValue("Sheet1", "D"+strconv.Itoa(i+2), "R$ "+strconv.FormatFloat(p.Price, 'f', 2, 64))
 		f.SetCellValue("Sheet1", "E"+strconv.Itoa(i+2), p.CreatedAt)
 		f.SetCellValue("Sheet1", "F"+strconv.Itoa(i+2), p.UpdatedAt)
+
+		err = f.SetCellStyle("Sheet1", "A"+strconv.Itoa(i+2), "F"+strconv.Itoa(i+2), styleBody)
+		if err != nil {
+			return
+		}
+	}
+
+	err = f.SetColWidth("Sheet1", "B", "F", 25)
+	if err != nil {
+		return
+	}
+	err = f.SetCellStyle("Sheet1", "A1", "F1", styleHeader)
+	if err != nil {
+		return
 	}
 
 	// Save spreadsheet by the given path.
